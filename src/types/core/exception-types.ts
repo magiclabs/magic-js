728c87db5441c2d9ab89c957c949bc3499268024
Ian K Smith <smithki707@gmail.com>
2020-02-27 15:50:20 -0700

    Remove unused error code from enum

--- src/types/core/exception-types.ts
+++ src/types/core/exception-types.ts
@@ -1,7 +1,6 @@
 export enum ErrorCode {
   MissingApiKey = 'MISSING_API_KEY',
   ModalNotReady = 'MODAL_NOT_READY',
-  InvalidArgument = 'INVALID_ARGUMENT',
   MalformedResponse = 'MALFORMED_RESPONSE',
 }
 
