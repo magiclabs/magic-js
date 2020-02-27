3612d7bb34d49b51631a68d968a6c85b7ab508b6
Ian K Smith <smithki707@gmail.com>
2020-02-27 13:43:23 -0700

    Add 'AuthModule' tests

---
+++ test/spec/modules/auth/loginWithMagicLink.spec.ts
@@ -0,0 +1,55 @@
+import '../../../setup';
+
+import browserEnv from '@ikscodes/browser-env';
+import test from 'ava';
+import sinon from 'sinon';
+import { getPayloadIdStub } from '../../../lib/stubs';
+import { createMagicSDK } from '../../../lib/factories';
+import { BaseModule } from '../../../../src/modules/base-module';
+
+test.beforeEach(t => {
+  browserEnv.restore();
+  (BaseModule as any).prototype.request = sinon.stub();
+});
+
+/**
+ * `AuthModule.loginWithMagicLink` with `email` parameter.
+ *
+ * Action Must:
+ * - Generate JSON RPC request payload with method `magic_auth_login_with_magic_link`
+ */
+test.serial('#01', async t => {
+  const magic = createMagicSDK();
+
+  const idStub = getPayloadIdStub();
+  idStub.returns(222);
+
+  await magic.auth.loginWithMagicLink({ email: 'test' });
+
+  const requestPayload = (magic.user as any).request.args[0][0];
+  t.is(requestPayload.jsonrpc, '2.0');
+  t.is(requestPayload.id, 222);
+  t.is(requestPayload.method, 'magic_auth_login_with_magic_link');
+  t.deepEqual(requestPayload.params, [{ email: 'test', showUI: true }]);
+});
+
+/**
+ * `AuthModule.loginWithMagicLink`  with `showUI` parameter.
+ *
+ * Action Must:
+ * - Generate JSON RPC request payload with method `magic_auth_get_access_token`
+ */
+test.serial('#02', async t => {
+  const magic = createMagicSDK();
+
+  const idStub = getPayloadIdStub();
+  idStub.returns(777);
+
+  await magic.auth.loginWithMagicLink({ email: 'test', showUI: false });
+
+  const requestPayload = (magic.user as any).request.args[0][0];
+  t.is(requestPayload.jsonrpc, '2.0');
+  t.is(requestPayload.id, 777);
+  t.is(requestPayload.method, 'magic_auth_login_with_magic_link');
+  t.deepEqual(requestPayload.params, [{ email: 'test', showUI: false }]);
+});
