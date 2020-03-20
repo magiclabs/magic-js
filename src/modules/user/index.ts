e8120fa11e9a655fabe1e9ca82ce88d8731ac80f
Ian K Smith <smithki707@gmail.com>
2020-03-20 16:16:13 -0600

    Add 'generateIdToken' method

--- src/modules/user/index.ts
+++ src/modules/user/index.ts
@@ -1,11 +1,22 @@
 import { BaseModule } from '../base-module';
-import { GetIdTokenConfiguration, MagicPayloadMethod, MagicUserMetadata } from '../../types';
+import {
+  GetIdTokenConfiguration,
+  MagicPayloadMethod,
+  MagicUserMetadata,
+  GenerateIdTokenConfiguration,
+} from '../../types';
 import { createJsonRpcRequestPayload } from '../../core/json-rpc';
 
 export class UserModule extends BaseModule {
   /** */
   public getIdToken(configuration?: GetIdTokenConfiguration) {
     const requestPayload = createJsonRpcRequestPayload(MagicPayloadMethod.GetIdToken, [configuration]);
+    return this.request<string>(requestPayload);
+  }
+
+  /** */
+  public generateIdToken(configuration?: GenerateIdTokenConfiguration) {
+    const requestPayload = createJsonRpcRequestPayload(MagicPayloadMethod.GenerateIdToken, [configuration]);
     return this.request<string>(requestPayload);
   }
 
--- src/types/modules/user-types.ts
+++ src/types/modules/user-types.ts
@@ -5,6 +5,15 @@ export interface GetIdTokenConfiguration {
   lifespan?: number;
 }
 
+export interface GenerateIdTokenConfiguration extends GetIdTokenConfiguration {
+  /**
+   * An optional piece of data to sign with the token. Note, however, that the
+   * unsigned data _will not_ be encoded in the token, only an encrypted
+   * signature of the data.
+   */
+  attachment?: string;
+}
+
 export interface MagicUserMetadata {
   id: string | null;
   publicAddress: string | null;
