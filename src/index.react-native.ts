7421e50b71e1bd96e92323f289dbeadea194ffad
Ian K Smith <smithki707@gmail.com>
2020-04-22 18:10:36 -0600

    Polyfill certain globals for React Native

--- src/index.react-native.ts
+++ src/index.react-native.ts
@@ -1,5 +1,23 @@
 // React Native entry-point
 
+import process from 'process';
+import { Buffer } from 'buffer';
+
+global.process = process;
+process.browser = false;
+
+if (typeof btoa === 'undefined') {
+  global.btoa = str => {
+    return Buffer.from(str, 'binary').toString('base64');
+  };
+}
+
+if (typeof atob === 'undefined') {
+  global.atob = b64Encoded => {
+    return Buffer.from(b64Encoded, 'base64').toString('binary');
+  };
+}
+
 export { MagicSDKReactNative as Magic } from './core/sdk';
 export {
   MagicSDKError as SDKError,
