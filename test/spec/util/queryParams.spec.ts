f98c584e1ca58646e36da26b7b338656f74d2283
Ian K Smith <smithki707@gmail.com>
2020-04-23 17:50:28 -0600

    Fix query param test

--- test/spec/util/queryParams.spec.ts
+++ test/spec/util/queryParams.spec.ts
@@ -1,10 +1,10 @@
 import test from 'ava';
 import { encodeQueryParameters } from '../../../src/util/query-params';
 
-test('Encodes query parameters as Base64 JSON string', async t => {
+test('Encodes query parameters as compressed, URL-safe, Base64 JSON string', async t => {
   const result = encodeQueryParameters({
     API_KEY: 'test',
   });
   t.is(typeof result, 'string');
-  t.is(result, 'eJyrVnIM8Iz3do1UslIqSS0uUaoFADMKBZ0=');
+  t.is(result, 'eJyrVnIM8Iz3do1UslIqSS0uUaoFADMKBZ0%3D');
 });
