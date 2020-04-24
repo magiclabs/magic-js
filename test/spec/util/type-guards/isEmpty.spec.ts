2b9ba743c68f109a1de93a47732ad0a9ddeeb222
Ian K Smith <smithki707@gmail.com>
2020-04-23 18:26:37 -0600

    Add remaining tests

---
+++ test/spec/util/type-guards/isEmpty.spec.ts
@@ -0,0 +1,18 @@
+import test from 'ava';
+import { isEmpty } from '../../../../src/util/type-guards';
+
+test('Given `undefined`, returns true', async t => {
+  t.true(isEmpty(undefined));
+});
+
+test('Given `null`, returns true', async t => {
+  t.true(isEmpty(null));
+});
+
+test('Given empty object, returns true', async t => {
+  t.true(isEmpty({}));
+});
+
+test('Given non-empty object, returns false', async t => {
+  t.false(isEmpty({ hello: 'world' }));
+});
