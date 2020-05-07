6815a469615f5c71c87dc6a694029507dcf17a79
Ian K Smith <smithki707@gmail.com>
2020-05-07 14:34:15 -0600

    Add some comments to explain actions in 'index.react-native.ts'

--- src/index.react-native.ts
+++ src/index.react-native.ts
@@ -1,17 +1,23 @@
 // React Native entry-point
 /* eslint-disable global-require */
 
+// We expect `global.process` to be a Node Process, so we have to replace it
+// here.
 global.process = require('process');
+
+// WHATWG URL requires global `Buffer` access.
 if (typeof global.Buffer === 'undefined') global.Buffer = require('buffer').Buffer;
 
 process.browser = false;
 
+// Web3 assumes a browser context, so we need to provide a `btoa` shim.
 if (typeof btoa === 'undefined') {
   global.btoa = str => {
     return Buffer.from(str, 'binary').toString('base64');
   };
 }
 
+// Web3 assumes a browser context, so we need to provide an `atob` shim.
 if (typeof atob === 'undefined') {
   global.atob = b64Encoded => {
     return Buffer.from(b64Encoded, 'base64').toString('binary');
