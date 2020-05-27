ef33cc8d6107d762633ecbebfc85f575740bd8dc
Ian K Smith <smithki707@gmail.com>
2020-05-26 22:52:25 -0600

    Disallow simultaneous EventEmitter/Promise interface access

--- packages/provider/src/util/promise-tools.ts
+++ packages/provider/src/util/promise-tools.ts
@@ -2,30 +2,14 @@ import { TypedEmitter, EventsDefinition } from './events';
 import { MagicSDKError, MagicRPCError } from '../core/sdk-exceptions';
 
 /**
- * An exact replication of the `Promise` interface with an updated return type
- * for each chaining method.
- */
-interface EventedPromiseChain<T, TEvents extends EventsDefinition> extends Promise<T> {
-  then<TResult1 = T, TResult2 = never>(
-    onfulfilled?: ((value: T) => TResult1 | PromiseLike<TResult1>) | undefined | null,
-    onrejected?: ((reason: any) => TResult2 | PromiseLike<TResult2>) | undefined | null,
-  ): PromiEvent<TResult1 | TResult2, TEvents>;
-
-  catch<TResult = never>(
-    onrejected?: ((reason: any) => TResult | PromiseLike<TResult>) | undefined | null,
-  ): PromiEvent<T | TResult, TEvents>;
-
-  finally(onfinally?: (() => void) | undefined | null): PromiEvent<T, TEvents>;
-}
-
-/**
  * A `Promise` and `EventEmitter` all in one!
  */
-export type PromiEvent<TResult, TEvents extends EventsDefinition = void> = EventedPromiseChain<
-  TResult,
-  TEvents extends void ? DefaultEvents<TResult> : TEvents & DefaultEvents<TResult>
-> &
-  TypedEmitter<TEvents extends void ? DefaultEvents<TResult> : TEvents & DefaultEvents<TResult>>;
+export type PromiEvent<TResult, TEvents extends EventsDefinition = void> = Promise<TResult> &
+  {
+    [P in keyof TypedEmitter]: TypedEmitter<
+      TEvents extends void ? DefaultEvents<TResult> : TEvents & DefaultEvents<TResult>
+    >[P];
+  };
 
 /**
  * Default events attached to every `PromiEvent`.
@@ -45,72 +29,55 @@ type AsyncPromiseExecutor<TResult> = (
 ) => void | Promise<void>;
 
 /**
- * Clone a `Promise` object by calling `promise.then()`
- */
-function clonePromise<T extends Promise<any>>(promise: T): T {
-  return promise.then() as T;
-}
-
-/**
  * Create a native JavaScript `Promise` overloaded with strongly-typed methods
  * from `EventEmitter`.
  */
 export function createPromiEvent<TResult, TEvents extends EventsDefinition = void>(
   executor: AsyncPromiseExecutor<TResult>,
-): PromiEvent<TResult, TEvents & DefaultEvents<TResult>> {
+): PromiEvent<TResult, TEvents extends void ? DefaultEvents<TResult> : TEvents & DefaultEvents<TResult>> {
   const promise = createAutoCatchingPromise(executor);
   const eventEmitter = new TypedEmitter<TEvents & DefaultEvents<TResult>>();
-
-  const promiEvent = () => {
-    return Object.assign(clonePromise(promise), {
-      then: createChainingPromiseMethod('then'),
-      catch: createChainingPromiseMethod('catch'),
-      finally: createChainingPromiseMethod('finally'),
-
-      addListener: createChainingEmitterMethod('addListener'),
-      on: createChainingEmitterMethod('on'),
-      once: createChainingEmitterMethod('once'),
-
-      off: createChainingEmitterMethod('off'),
-      removeAllListeners: createChainingEmitterMethod('removeAllListeners'),
-      removeListener: createChainingEmitterMethod('removeListener'),
-
-      emit: createBoundEmitterMethod('emit'),
-      eventNames: createBoundEmitterMethod('eventNames'),
-      listeners: createBoundEmitterMethod('listeners'),
-      listenerCount: createBoundEmitterMethod('listenerCount'),
-    });
-  };
-
-  const createChainingPromiseMethod = (method: keyof typeof promise) => (...args: any[]) => {
-    (promise as any)[method].apply(promise, args);
-    return promiEvent();
-  };
-
-  const createChainingEmitterMethod = (method: keyof typeof eventEmitter) => (...args: any[]) => {
-    (eventEmitter as any)[method].apply(eventEmitter, args);
-    return promiEvent();
-  };
+  let isUsingPromise = true;
 
   const createBoundEmitterMethod = (method: keyof typeof eventEmitter) => (...args: any[]) => {
     return (eventEmitter as any)[method].apply(eventEmitter, args);
   };
 
-  const result = promiEvent();
+  const createChainingEmitterMethod = (method: keyof typeof eventEmitter) => (...args: any[]) => {
+    isUsingPromise = false;
+    return createBoundEmitterMethod(method)(...args);
+  };
 
-  result.then(resolved => {
-    result.emit('done', resolved);
-    if (!promise.finally) result.emit('settled');
+  const source = promise.then(
+    resolved => {
+      promiEvent.emit('done', resolved);
+      promiEvent.emit('settled');
+      return resolved;
+    },
+
+    err => {
+      promiEvent.emit('error', err);
+      promiEvent.emit('settled');
+      if (isUsingPromise) throw err;
+    },
+  );
+
+  const promiEvent = Object.assign(source, {
+    addListener: createChainingEmitterMethod('addListener'),
+    on: createChainingEmitterMethod('on'),
+    once: createChainingEmitterMethod('once'),
+
+    off: createChainingEmitterMethod('off'),
+    removeAllListeners: createChainingEmitterMethod('removeAllListeners'),
+    removeListener: createChainingEmitterMethod('removeListener'),
+
+    emit: createBoundEmitterMethod('emit'),
+    eventNames: createBoundEmitterMethod('eventNames'),
+    listeners: createBoundEmitterMethod('listeners'),
+    listenerCount: createBoundEmitterMethod('listenerCount'),
   });
 
-  result.catch(err => {
-    result.emit('error', err);
-    if (!promise.finally) result.emit('settled');
-  });
-
-  if ((promise as any).finally) result.finally(() => result.emit('settled'));
-
-  return result as any;
+  return promiEvent as any;
 }
 
 /**
