0033572f9345941974c28f761bd14c871b240479
Ian K Smith <smithki707@gmail.com>
2020-07-13 21:55:45 -0600

    Fix dev script bug

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -6,12 +6,13 @@ echo "  Building for development..."
 echo
 
 define_paths=$(echo -e $(yarn --silent paths))
-project_paths=$(echo -e $(yarn --silent paths tsconfig.json)) $(echo -e $(yarn --silent paths tsconfig.module.json))
+project_paths_cjs=$(echo -e $(yarn --silent paths tsconfig.json))
+project_paths_esm=$(echo -e $(yarn --silent paths tsconfig.module.json))
 
 echo "Compiling TypeScripts for the following projects:"
-node -pe "'$project_paths'.split(' ').reduce((prev, next) => prev + '\n - ' + next, '')"
+node -pe "'$project_paths_cjs $project_paths_esm'.split(' ').reduce((prev, next) => prev + '\n - ' + next, '')"
 echo
 
 sleep 3
 
-tsc-watch --onSuccess "$INIT_CWD/scripts/env.ts $define_paths" -b -w $project_paths
+tsc-watch --onSuccess "$INIT_CWD/scripts/env.ts $define_paths" -b -w $project_paths_cjs $project_paths_esm
