6d02765ebf9ea50c285d7529f08bd3cb180f4053
Ian K Smith <smithki707@gmail.com>
2020-07-13 20:30:02 -0600

    Add increased 'ava' timeout to prevent CircleCI test failures

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,6 +5,9 @@
 
 RAW_INPUT=$1
 
+# Increase memory limit for Node
+export NODE_OPTIONS=--max_old_space_size=4096
+
 runTests() {
   # Parse a glob of input test files (relative to the package directory).
   input=$(echo $($INIT_CWD/scripts/glob.ts $RAW_INPUT))
@@ -12,7 +15,7 @@ runTests() {
   export TS_NODE_PROJECT="./test/tsconfig.json"
 
   # Run tests, with coverage.
-  npx nyc --reporter=lcov --reporter=text-summary  ava $input || exit 1
+  npx nyc --reporter=lcov --reporter=text-summary  ava -T 120000 $input || exit 1
 }
 
 echoNoTests() {
