1067a7676ef5d187af10494b07ed72da786164b7
Ian K Smith <smithki707@gmail.com>
2020-09-08 14:53:17 -0600

    Remove 'postInstall.sh' script, replace with Yarn's 'nohoist' config

--- scripts/postInstall.sh
+++
@@ -1,6 +0,0 @@
-# Install process and buffer into React-native node_modules, so it get published
-
-cd ./packages/react-native || echo "Failed to install React-native dependencies"
-npm install buffer@$(node -p -e "require('./package.json').dependencies.buffer")
-npm install process@$(node -p -e "require('./package.json').dependencies.process")
-
