61997a46ced5e0306a1dffa7a2be4ac3d0132983
Ian K Smith <smithki@users.noreply.github.com>
2020-10-25 19:15:21 -0600

    Simplify scripts (#147)
    * Use 'wsrun' for pre-commit hook
    
    * Add '-c' flag to 'paths' script in package.json

--- scripts/runner.sh
+++ scripts/runner.sh
@@ -5,8 +5,6 @@
 
 export PATH=$PWD/node_modules/.bin:$INIT_CWD/node_modules/.bin:$PATH
 
-VERBOSE='1'
-if [ "$1" == 'silent' ]; then VERBOSE=''; shift; fi
 if [ "$1" == 'run' ]; then shift; fi
 
 RUNCMD=$(node -pe "require('$INIT_CWD/package.json').scripts['$1']")
@@ -17,5 +15,4 @@ then
 fi
 
 BASHCMD="$RUNCMD ${@:2}"
-if [ $VERBOSE ]; then echo '$' $BASHCMD; fi;
 exec bash -c "$BASHCMD"
