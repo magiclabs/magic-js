637c72db4d135c70b64ca58204335361857650bd
Ian K Smith <smithki@users.noreply.github.com>
2020-12-01 13:22:10 -0700

    Add 'importHelpers: true' to base tsconfig.json (#152)

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,10 +4,11 @@ echo
 boxen --border-color cyan --dim-border --padding 1 "Building TypeScripts..."
 echo
 
+echo "🤔 Determining TypeScript projects to build..."
 tsconfig_paths=$(echo -e $(yarn --silent paths tsconfig.json tsconfig.module.json))
 
-echo "Compiling TypeScripts for the following projects:"
-node -pe "'$tsconfig_paths'.split(' ').reduce((prev, next) => prev + '\n  - ' + next, '')"
+echo "  Compiling TypeScripts for the following projects:"
+node -pe "'$tsconfig_paths'.split(' ').reduce((prev, next) => prev + '\n    - ' + next, '')"
 echo
 
 tsc -b $tsconfig_paths
@@ -17,9 +18,6 @@ echo "TypeScripts compiled."
 
 echo
 boxen --border-color cyan --dim-border --padding 1 "Building CDN bundles..."
-echo
-
-echo "You can safely ignore \`The 'this' keyword is equivalent to 'undefined'\` warnings"
 echo
 
 yarn wsrun --serial $INIT_CWD/scripts/wsrun/build:cdn.sh
--- scripts/dev.sh
+++ scripts/dev.sh
@@ -4,13 +4,15 @@ echo
 boxen --border-color cyan --dim-border --padding 1 "Building for development..."
 echo
 
+echo "🤔 Determining TypeScript projects to build..."
 pkg_paths=$(echo -e $(yarn --silent paths))
 tsconfig_paths=$(echo -e $(yarn --silent paths tsconfig.json tsconfig.module.json))
 
-echo "Compiling TypeScripts for the following projects:"
-node -pe "'$tsconfig_paths'.split(' ').reduce((prev, next) => prev + '\n  - ' + next, '')"
+
+echo "  Compiling TypeScripts for the following projects:"
+node -pe "'$tsconfig_paths'.split(' ').reduce((prev, next) => prev + '\n    - ' + next, '')"
 echo
 
-sleep 3
+sleep 5
 
 tsc-watch --onSuccess "$INIT_CWD/scripts/inject-env.ts $pkg_paths" -b -w $tsconfig_paths
