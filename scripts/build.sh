6e692dfe4f8879608a498bfc4e6e57bb861a54f6
Ian K Smith <22753882+smithki@users.noreply.github.com>
2021-07-28 13:16:31 -0600

    Add explicit JSDelivr entry-point for `magic-sdk` (#191)
    * Update build system to prioritize ES modules
    
    * Prevent TS build info files from being published to NPM

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,7 +5,7 @@ boxen --border-color cyan --dim-border --padding 1 "Building TypeScripts..."
 echo
 
 echo "🤔 Determining TypeScript projects to build..."
-tsconfig_paths=$(echo -e $(yarn --silent paths tsconfig.json tsconfig.module.json))
+tsconfig_paths=$(echo -e $(yarn --silent paths tsconfig.json tsconfig.cjs.json))
 
 echo "  Compiling TypeScripts for the following projects:"
 node -pe "'$tsconfig_paths'.split(' ').reduce((prev, next) => prev + '\n    - ' + next, '')"
