6118c7479d489e7a4123eede1612288304074b8f
Ian K Smith <smithki707@gmail.com>
2021-09-27 18:09:22 -0600

    Update PnP implementation to include hooks for login, logout, and login callback

---
+++ packages/@magic-sdk/pnp/src/context/login.ts
@@ -0,0 +1,29 @@
+import { createMagicInstance, getScriptData } from '../utils';
+
+export async function login(): Promise<void> {
+  const { src, apiKey, redirectURI = `${window.location.origin}/callback` } = getScriptData();
+
+  const magic = createMagicInstance(apiKey, src.origin);
+
+  async function handleOAuthLogin(provider: any) {
+    await magic.oauth.loginWithRedirect({ provider, redirectURI });
+  }
+
+  async function handleEmailLinkLogin(email: any) {
+    const didt = await magic.auth.loginWithMagicLink({ email, redirectURI });
+    window.location.href = `${redirectURI}?didt=${encodeURIComponent(didt!)}`;
+  }
+
+  const [loginType, arg] = await magic.pnp.getLoginMethod();
+
+  switch (loginType) {
+    case 'oauth':
+      return handleOAuthLogin(arg).catch(login);
+
+    case 'email-link':
+      return handleEmailLinkLogin(arg).catch(login);
+
+    default:
+      return login();
+  }
+}
