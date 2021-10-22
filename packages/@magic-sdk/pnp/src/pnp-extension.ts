af0a37d3f457b4d61fa84a17bb278d078159814d
Ian K Smith <22753882+smithki@users.noreply.github.com>
2021-10-22 12:41:41 -0600

    Add `UserModule#settings` endpoint (#231)
    * Add 'UserModule#settings' endpoint
    
    * Add support for user settings via PnP-based implementation
    
    * Clear URL query upon settings callback
    
    * Update 'magic.user.settings' method name to 'magic.user.showSettings'
    
    * Add PnP-specific settings endpoint
    
    * Update yarn.lock

--- packages/@magic-sdk/pnp/src/pnp-extension.ts
+++ packages/@magic-sdk/pnp/src/pnp-extension.ts
@@ -1,13 +1,15 @@
+import type { MagicUserMetadata } from '@magic-sdk/types';
+
 export class PlugNPlayExtension extends window.Magic.Extension.Internal<'pnp', { isPnP: boolean }> {
   config = { isPnP: true };
   name = 'pnp' as const;
 
-  static storageKeys = {
+  public static storageKeys = {
     lastUsedProvider: 'pnp/lastUsedProvider',
   };
 
-  getLoginMethod(debug?: boolean) {
-    return this.utils.createPromiEvent<[string, string | undefined], { yolo: () => void }>(async (resolve) => {
+  public getLoginMethod(debug?: boolean) {
+    return this.utils.createPromiEvent<[string, string | undefined]>(async (resolve) => {
       const lastUsedProvider = await this.utils.storage.getItem<string | undefined>(
         PlugNPlayExtension.storageKeys.lastUsedProvider,
       );
@@ -16,9 +18,22 @@ export class PlugNPlayExtension extends window.Magic.Extension.Internal<'pnp', {
     });
   }
 
-  async saveLastUsedProvider(provider?: string) {
+  public showSettings() {
+    return this.request(this.utils.createJsonRpcRequestPayload('pnp/settings'));
+  }
+
+  public async saveLastUsedProvider(provider?: string) {
     if (provider) {
       await this.utils.storage.setItem(PlugNPlayExtension.storageKeys.lastUsedProvider, provider);
     }
+  }
+
+  public encodeUserMetadata(userMetadata: MagicUserMetadata) {
+    return this.utils.encodeJSON(userMetadata);
+  }
+
+  public decodeUserMetadata(userMetadataQueryString?: string | null): MagicUserMetadata | null {
+    if (!userMetadataQueryString) return null;
+    return this.utils.decodeJSON(userMetadataQueryString);
   }
 }
---
+++ packages/@magic-sdk/pnp/src/utils/magic-instance.ts
@@ -0,0 +1,20 @@
+import { PlugNPlayExtension } from '../pnp-extension';
+import type { Magic, OAuthExtension } from '../types';
+
+export function createMagicInstance(apiKey?: string, endpoint?: string): Magic<[PlugNPlayExtension, OAuthExtension]> {
+  const extensions = removeFalsey([
+    new PlugNPlayExtension(),
+    window.MagicOAuthExtension && new window.MagicOAuthExtension(),
+  ]);
+
+  return new window.Magic(apiKey!, {
+    endpoint,
+    extensions,
+  });
+}
+
+export type PNPMagicInstance = ReturnType<typeof createMagicInstance>;
+
+function removeFalsey<T>(arr: T[]): Array<NonNullable<T>> {
+  return arr.filter(Boolean) as unknown as Array<NonNullable<T>>;
+}
