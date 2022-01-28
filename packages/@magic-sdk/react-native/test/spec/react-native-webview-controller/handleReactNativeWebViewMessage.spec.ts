e423dd9c9d9c730913a22a895409b68d7acdfc38
Ethella <jerry@fortmatic.com>
2022-01-28 11:39:41 -0800

    * Fix tests coverage

--- packages/@magic-sdk/react-native/test/spec/react-native-webview-controller/handleReactNativeWebViewMessage.spec.ts
+++ packages/@magic-sdk/react-native/test/spec/react-native-webview-controller/handleReactNativeWebViewMessage.spec.ts
@@ -95,3 +95,73 @@ test('Ignores event handlers where `messageHandlers` size is === 0', (done) => {
     done();
   }, 100);
 });
+
+test('Process Typed Array in Solana Payload', (done) => {
+  const viewController = createReactNativeWebViewController('asdf');
+  const onHandlerStub = jest.fn();
+
+  viewController.messageHandlers.add(onHandlerStub);
+
+  viewController.handleReactNativeWebViewMessage({
+    nativeEvent: {
+      url: `asdf/send/?params=${ENCODED_QUERY_PARAMS}`,
+      data: JSON.stringify({
+        msgType: `asdf-${ENCODED_QUERY_PARAMS}`,
+        response: {
+          result: {
+            rawTransaction: {
+              constructor: 'Uint8Array',
+              data: '21,0,0,0',
+              flag: 'MAGIC_PAYLOAD_FLAG_TYPED_ARRAY',
+            },
+          },
+        },
+      }),
+    },
+  } as any);
+
+  setTimeout(() => {
+    expect(onHandlerStub).toBeCalledTimes(1);
+    expect(onHandlerStub.mock.calls[0][0].data).toEqual({
+      msgType: `asdf-${ENCODED_QUERY_PARAMS}`,
+      response: { result: { rawTransaction: new Uint8Array([21, 0, 0, 0]) } },
+    });
+    done();
+  }, 100);
+});
+
+test('Process Typed Array in Solana Payload', (done) => {
+  const viewController = createReactNativeWebViewController('asdf');
+  const onHandlerStub = jest.fn();
+
+  viewController.messageHandlers.add(onHandlerStub);
+
+  const unrecognizedObject = {
+    constructor: 'Uint8Array',
+    data: ['21,0,0,0'],
+    flag: 'MAGIC_PAYLOAD_FLAG_TYPED_ARRAY',
+  };
+
+  viewController.handleReactNativeWebViewMessage({
+    nativeEvent: {
+      url: `asdf/send/?params=${ENCODED_QUERY_PARAMS}`,
+      data: JSON.stringify({
+        msgType: `asdf-${ENCODED_QUERY_PARAMS}`,
+        response: {
+          result: {
+            rawTransaction: unrecognizedObject,
+          },
+        },
+      }),
+    },
+  } as any);
+
+  setTimeout(() => {
+    expect(onHandlerStub).toBeCalledTimes(1);
+    expect(onHandlerStub.mock.calls[0][0].data).toEqual({
+      msgType: `asdf-${ENCODED_QUERY_PARAMS}`,
+      response: { result: { rawTransaction: unrecognizedObject } },
+    });
+    done();
+  }, 100);
+});
