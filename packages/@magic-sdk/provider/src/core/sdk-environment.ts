c35aa1d15fa25291bdd2bea8195abb4a277afce0
Jerry Liu <33166884+Ethella@users.noreply.github.com>
2022-07-26 21:47:45 -0700

    Revert "Enable bundle Id in Expo (#335)" (#337)
    This reverts commit d6d9987be7f7a015d8696c8e5b469a32575d18bc.

--- packages/@magic-sdk/provider/src/core/sdk-environment.ts
+++ packages/@magic-sdk/provider/src/core/sdk-environment.ts
@@ -16,7 +16,6 @@ export interface SDKEnvironment {
   defaultEndpoint: string;
   ViewController: ConstructorOf<ViewController>;
   configureStorage: () => Promise<typeof localForage>;
-  bundleId?: string | null;
 }
 
 export const SDKEnvironment: SDKEnvironment = {} as any;
