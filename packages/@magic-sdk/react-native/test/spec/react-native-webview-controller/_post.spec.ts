5bcb7ee1d206ea857e4b8a086d18d650e537d607
Jerry Liu <33166884+Ethella@users.noreply.github.com>
2022-09-08 16:44:00 -0700

    Add RN TypedArray stringify conversion logic (#357)
    * Add RN TypedArray stringify conversion logic
    
    * Add test

--- packages/@magic-sdk/react-native/test/spec/react-native-webview-controller/_post.spec.ts
+++ packages/@magic-sdk/react-native/test/spec/react-native-webview-controller/_post.spec.ts
@@ -28,3 +28,25 @@ test('Throws MODAL_NOT_READY error if webView is nil', async () => {
 
   expect(() => overlay._post({ thisIsData: 'hello world' })).rejects.toThrow(expectedError);
 });
+
+test('Process Typed Array in a Solana Request', async () => {
+  const overlay = createReactNativeWebViewController('http://example.com');
+
+  const postStub = jest.fn();
+  overlay.webView = { postMessage: postStub };
+
+  await overlay._post({
+    msgType: 'MAGIC_HANDLE_REQUEST-troll',
+    payload: {
+      id: 3,
+      jsonrpc: '2.0',
+      method: 'sol_signMessage',
+      params: { message: new Uint8Array([72, 101, 108, 108, 111]) },
+    },
+  });
+
+  expect(postStub.mock.calls[0]).toEqual([
+    '{"msgType":"MAGIC_HANDLE_REQUEST-troll","payload":{"id":3,"jsonrpc":"2.0","method":"sol_signMessage","params":{"message":{"constructor":"Uint8Array","data":"72,101,108,108,111","flag":"MAGIC_PAYLOAD_FLAG_TYPED_ARRAY"}}}}',
+    'http://example.com',
+  ]);
+});
