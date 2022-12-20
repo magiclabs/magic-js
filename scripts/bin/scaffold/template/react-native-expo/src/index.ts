bc08f99ccbfea85608228f6aab80f201dbd33f48
Arian Flores <arian.flores@magic.link>
2022-12-19 17:00:47 -0800

    Remove unecessary scaffold change

--- scripts/bin/scaffold/template/react-native-expo/src/index.ts
+++
@@ -1,6 +0,0 @@
-import { Extension } from '@magic-sdk/react-native-expo';
-
-export class <%= className %> extends Extension.Internal<'<%= extNameCamelCase %>', any> {
-  name = '<%= extNameCamelCase %>' as const;
-  config: any = {};
-}
