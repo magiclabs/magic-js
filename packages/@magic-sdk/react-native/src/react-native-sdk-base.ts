e82e1fe324940f99b4678a0b58b823c7fcfd14c9
Arian Flores <arian.flores@magic.link>
2023-01-03 10:58:22 -0800

    Removes Legacy @magic-sdk/react-native package

--- packages/@magic-sdk/react-native/src/react-native-sdk-base.ts
+++
@@ -1,8 +0,0 @@
-import { SDKBase } from '@magic-sdk/provider';
-import { ReactNativeWebViewController } from './react-native-webview-controller';
-
-export class SDKBaseReactNative extends SDKBase {
-  public get Relayer() {
-    return (this.overlay as ReactNativeWebViewController).Relayer;
-  }
-}
