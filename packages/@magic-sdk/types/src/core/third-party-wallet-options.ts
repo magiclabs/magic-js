5377aacccb9ca1fe513169fb01583292f05a64bd
Hunter Cote <huntercote2@gmail.com>
2023-03-10 09:36:24 -0500

    add coinbasewallet.sdk type

--- packages/@magic-sdk/types/src/core/third-party-wallet-options.ts
+++ packages/@magic-sdk/types/src/core/third-party-wallet-options.ts
@@ -1,6 +1,6 @@
 export interface ThirdPartyWalletOptions {
   coinbaseWallet?: {
-    sdk: any;
+    sdk: CoinbaseWalletSDKOptions;
     provider: {
       jsonRpcUrl?: string;
       chainId?: number;
