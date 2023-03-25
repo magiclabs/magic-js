4c6d8a0d5d84afe9728232af618c399629a0ada0
Arian Flores <arian.flores@magic.link>
2023-03-24 19:47:56 -0700

    Revert "move third party wallet connections to sdk (#473)"
    This reverts commit 5e3070afa76eed45d3c841dbc31aef55b5fee8ca.

--- packages/@magic-sdk/provider/test/spec/modules/wallet/connectWithUI.spec.ts
+++ packages/@magic-sdk/provider/test/spec/modules/wallet/connectWithUI.spec.ts
@@ -1,125 +1,23 @@
 import browserEnv from '@ikscodes/browser-env';
-import { CoinbaseWalletSDK } from '@coinbase/wallet-sdk';
 import { createMagicSDK } from '../../../factories';
-
-jest.mock('@coinbase/wallet-sdk');
+import { isPromiEvent } from '../../../../src/util';
 
 beforeEach(() => {
-  browserEnv({ url: 'http://localhost' });
   browserEnv.restore();
 });
 
-test('Generate JSON RPC request payload with method `mc_login` and `env` params as an object', async () => {
+test('Generate JSON RPC request payload with method `eth_requestAccounts`', async () => {
   const magic = createMagicSDK();
-  magic.wallet.request = jest.fn(() => {
-    return {
-      on: () => '',
-    };
-  });
+  magic.wallet.request = jest.fn();
 
-  await magic.wallet.connectWithUI();
+  magic.wallet.connectWithUI();
+
   const requestPayload = magic.wallet.request.mock.calls[0][0];
-
-  expect(requestPayload.method).toBe('mc_login');
-  expect(requestPayload.params).toEqual([
-    {
-      env: {
-        isCoinbaseWalletInstalled: false,
-        isMetaMaskInstalled: false,
-      },
-    },
-  ]);
+  expect(requestPayload.method).toBe('eth_requestAccounts');
+  expect(requestPayload.params).toEqual([]);
 });
 
-test('auto-connect if metamask browser', async () => {
+test('method should return a PromiEvent', () => {
   const magic = createMagicSDK();
-  magic.wallet.request = jest.fn(() => {
-    return {
-      on: () => null,
-    };
-  });
-
-  magic.wallet.isMetaMaskBrowser = jest.fn(() => true);
-
-  jest.mock('@magic-sdk/provider/src/core/json-rpc.ts', () => {
-    return {
-      createJsonRpcRequestPayload: () => true,
-    };
-  });
-
-  const provider = {
-    isMetaMask: true,
-    request: async (request: { method: string; params?: Array<any> }) => {
-      if (request.method === 'eth_requestAccounts') {
-        return ['0x0000000000000000000000000000000000000000'];
-      }
-      return '';
-    },
-  };
-  window.ethereum = provider;
-
-  await magic.wallet.connectWithUI();
-
-  const requestPayload = magic.wallet.request.mock.calls[0][0];
-  expect(requestPayload.method).toBe('mc_is_wallet_enabled');
-  expect(requestPayload.params).toEqual([{ wallet: 'metamask' }]);
-});
-
-test('auto-connect if coinbase wallet browser', async () => {
-  const magic = createMagicSDK({
-    thirdPartyWalletOptions: {
-      coinbaseWallet: {
-        sdk: {
-          appName: 'Magic Test',
-          appLogoUrl: '',
-          darkMode: false,
-        },
-        provider: {
-          jsonRpcUrl: '',
-          chainId: 1,
-        },
-      },
-    },
-  });
-  magic.wallet.request = jest.fn(() => {
-    return {
-      on: () => null,
-    };
-  });
-
-  magic.wallet.isCoinbaseWalletBrowser = jest.fn(() => true);
-
-  jest.mock('@magic-sdk/provider/src/core/json-rpc.ts', () => {
-    return {
-      createJsonRpcRequestPayload: () => true,
-    };
-  });
-
-  jest.spyOn(CoinbaseWalletSDK.prototype, 'makeWeb3Provider').mockImplementation(() => {
-    return {
-      request: async (request: { method: string; params?: Array<any> }) => {
-        if (request.method === 'eth_requestAccounts') {
-          return ['0x0000000000000000000000000000000000000000'];
-        }
-        return '';
-      },
-    } as any;
-  });
-
-  const provider = {
-    isCoinbaseWalletBrowser: true,
-    request: async (request: { method: string; params?: Array<any> }) => {
-      if (request.method === 'eth_requestAccounts') {
-        return ['0x0000000000000000000000000000000000000000'];
-      }
-      return '';
-    },
-  };
-  window.ethereum = provider;
-
-  await magic.wallet.connectWithUI();
-
-  const requestPayload = magic.wallet.request.mock.calls[0][0];
-  expect(requestPayload.method).toBe('mc_is_wallet_enabled');
-  expect(requestPayload.params).toEqual([{ wallet: 'coinbase_wallet' }]);
+  expect(isPromiEvent(magic.wallet.connectWithUI())).toBeTruthy();
 });
