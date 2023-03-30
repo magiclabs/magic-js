cbbd63f88f990a172f2bed63d9fb3bd3e33e7af3
ethella <jerry@magic.link>
2023-03-29 20:19:50 -0700

    * Add cancel event

--- packages/@magic-sdk/types/src/modules/intermediary-types.ts
+++ packages/@magic-sdk/types/src/modules/intermediary-types.ts
@@ -1,8 +1,12 @@
 import { LoginWithEmailOTPEvents, LoginWithMagicLinkEvents } from './auth-types';
 
-export type IntermediaryEvents = keyof LoginWithEmailOTPEvents | keyof LoginWithMagicLinkEvents;
+export type IntermediaryEvents = keyof LoginWithEmailOTPEvents | keyof LoginWithMagicLinkEvents | keyof GeneralEvents;
 export type IntermediaryEventPayload = {
   payloadId: string;
   eventType: IntermediaryEvents;
   args: any;
+};
+
+export type GeneralEvents = {
+  cancel: () => void;
 };
