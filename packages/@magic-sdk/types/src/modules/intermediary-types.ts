c84d635eeef7b3b9dd3354eb7c576d238cea8511
ethella <jerry@magic.link>
2023-03-29 22:11:14 -0700

    remove general events

--- packages/@magic-sdk/types/src/modules/intermediary-types.ts
+++ packages/@magic-sdk/types/src/modules/intermediary-types.ts
@@ -1,12 +1,8 @@
 import { LoginWithEmailOTPEvents, LoginWithMagicLinkEvents } from './auth-types';
 
-export type IntermediaryEvents = keyof LoginWithEmailOTPEvents | keyof LoginWithMagicLinkEvents | keyof GeneralEvents;
+export type IntermediaryEvents = keyof LoginWithEmailOTPEvents | keyof LoginWithMagicLinkEvents;
 export type IntermediaryEventPayload = {
   payloadId: string;
   eventType: IntermediaryEvents;
   args: any;
-};
-
-export type GeneralEvents = {
-  cancel: () => void;
 };
