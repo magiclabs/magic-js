d9b57b00394add5f2594b3351cebc46cf334a980
ethella <jerry@magic.link>
2023-04-06 07:16:06 -0700

    add custom Auth implementation

--- packages/@magic-ext/custom-auth/src/index.ts
+++ packages/@magic-ext/custom-auth/src/index.ts
@@ -1,6 +1,12 @@
 import { Extension } from '@magic-sdk/commons';
+import { CustomAuthPayloadMethod } from '@magic-ext/connect';
 
 export class CustomAuth extends Extension.Internal<'customAuth', any> {
   name = 'customAuth' as const;
   config: any = {};
+
+  public setAuthorizationJWT() {
+    const requestPayload = this.utils.createJsonRpcRequestPayload(CustomAuthPayloadMethod.SetAuthorizationJwt);
+    return this.request<boolean>(requestPayload);
+  }
 }
