b7494b61dac3fa467f64bef96c489801663f8311
David He <david.he@magic.link>
2023-04-20 13:04:38 -0400

    remove unused type

--- packages/@magic-ext/auth/src/types.ts
+++ packages/@magic-ext/auth/src/types.ts
@@ -5,10 +5,3 @@ export interface UserInfo {
 export interface WalletInfo {
   walletType: 'magic' | 'metamask' | 'coinbase_wallet' | 'wallet_connect';
 }
-
-export enum MagicConnectPayloadMethod {
-  GetWalletInfo = 'mc_get_wallet_info',
-  ShowWallet = 'mc_wallet',
-  RequestUserInfo = 'mc_request_user_info',
-  Disconnect = 'mc_disconnect',
-}
