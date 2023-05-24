70b3bfcfb7eb03bbfaafa10e2993561b50c8b5a7
Jay Hwang <octave08b@gmail.com>
2023-05-25 06:38:47 +0900

    Refactor aptos config

--- packages/@magic-ext/aptos/src/type.ts
+++ packages/@magic-ext/aptos/src/type.ts
@@ -1,16 +1,5 @@
 export interface AptosConfig {
   nodeUrl: string;
-  rpcUrl?: string;
-  network?: string;
-  chainId?: number;
-}
-
-export interface ConfigType {
-  rpcUrl: string;
-  nodeUrl: string;
-  network: string;
-  chainType: string;
-  chainId: number;
 }
 
 export enum AptosPayloadMethod {
