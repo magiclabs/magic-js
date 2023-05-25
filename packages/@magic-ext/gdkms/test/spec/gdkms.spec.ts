55f8c93db375f2afeb4fd84da872045caafaca57
ethella <jerry@magic.link>
2023-05-25 12:15:10 -0700

    Add configuration to enable testing framework for extension packages

--- packages/@magic-ext/gdkms/test/spec/gdkms.spec.ts
+++ packages/@magic-ext/gdkms/test/spec/gdkms.spec.ts
@@ -1,33 +1,35 @@
 import browserEnv from '@ikscodes/browser-env';
 import { MagicPayloadMethod } from '@magic-sdk/types';
-import { createMagicSDK } from '../../../factories';
+import { createMagicSDK, createMagicSDKWithExtension } from '../../../../@magic-sdk/provider/test/factories';
+import { GDKMSExtension } from '../../src';
 
 beforeEach(() => {
   browserEnv.restore();
 });
 
 test('Construct Encrypt Request with `magic_auth_encrypt_v1`', async () => {
-  const magic = createMagicSDK();
-  magic.user.request = jest.fn();
+  const magic = createMagicSDKWithExtension({}, [new GDKMSExtension()]);
+  console.log('magic', magic);
+  magic.gdkms.request = jest.fn();
 
   const message = 'hello world';
 
-  magic.user.encryptWithPrivateKey(message);
+  magic.gdkms.encryptWithPrivateKey(message);
 
-  const requestPayload = magic.user.request.mock.calls[0][0];
+  const requestPayload = magic.gdkms.request.mock.calls[0][0];
   expect(requestPayload.method).toBe(MagicPayloadMethod.EncryptV1);
   expect(requestPayload.params).toEqual([{ message }]);
 });
 
 test('Construct Decrypt Request with `magic_auth_decrypt_v1`', async () => {
-  const magic = createMagicSDK();
-  magic.user.request = jest.fn();
+  const magic = createMagicSDKWithExtension({}, [new GDKMSExtension()]);
+  magic.gdkms.request = jest.fn();
 
   const cipherText = 'XJD/1238t';
 
-  magic.user.decryptWithPrivateKey(cipherText);
+  magic.gdkms.decryptWithPrivateKey(cipherText);
 
-  const requestPayload = magic.user.request.mock.calls[0][0];
+  const requestPayload = magic.gdkms.request.mock.calls[0][0];
   expect(requestPayload.method).toBe(MagicPayloadMethod.DecryptV1);
   expect(requestPayload.params).toEqual([{ cipherText }]);
 });
