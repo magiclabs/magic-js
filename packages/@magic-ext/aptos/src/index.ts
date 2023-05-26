88238c42b562ce97ff09b06a8736f3fa3a2b71fb
Jay Hwang <octave08b@gmail.com>
2023-05-27 05:34:18 +0900

    Refactor aptos config (#524)
    * Refactor aptos config
    
    * Update yarn.lock

--- packages/@magic-ext/aptos/src/index.ts
+++ packages/@magic-ext/aptos/src/index.ts
@@ -3,51 +3,21 @@ import { Extension } from '@magic-sdk/commons';
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 import { BCS } from 'aptos';
-import { AptosConfig, ConfigType, AptosPayloadMethod } from './type';
-
-const getDefaultConfig = (nodeUrl: string) => {
-  if (nodeUrl === 'https://fullnode.mainnet.aptoslabs.com') {
-    return {
-      rpcUrl: 'https://aptos-mainnet-rpc.allthatnode.com/v1',
-      network: 'mainnet',
-      chainId: 1,
-    };
-  }
-
-  if (nodeUrl === 'https://fullnode.testnet.aptoslabs.com') {
-    return {
-      rpcUrl: 'https://aptos-testnet-rpc.allthatnode.com/v1',
-      network: 'testnet',
-      chainId: 2,
-    };
-  }
-
-  if (nodeUrl === 'https://fullnode.devnet.aptoslabs.com') {
-    return {
-      rpcUrl: '',
-      network: 'devnet',
-      chainId: 54,
-    };
-  }
-
-  throw new Error('Invalid nodeUrl');
-};
+import { AptosConfig, AptosPayloadMethod } from './type';
 
 export class AptosExtension extends Extension.Internal<'aptos', any> {
   name = 'aptos' as const;
-  config: ConfigType;
+  config: any = {};
 
   constructor(public aptosConfig: AptosConfig) {
     super();
 
-    const defaultConfig = getDefaultConfig(aptosConfig.nodeUrl);
-
     this.config = {
+      rpcUrl: '',
       chainType: 'APTOS',
-      nodeUrl: aptosConfig.nodeUrl,
-      rpcUrl: aptosConfig.rpcUrl ?? defaultConfig.rpcUrl,
-      network: aptosConfig.network ?? defaultConfig.network,
-      chainId: aptosConfig.chainId ?? defaultConfig.chainId,
+      options: {
+        nodeUrl: aptosConfig.nodeUrl,
+      },
     };
   }
 
