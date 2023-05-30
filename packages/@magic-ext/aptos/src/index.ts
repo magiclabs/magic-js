7cbe259afbe2090120e08e776e99461cb6ec122a
Jay Hwang <octave08b@gmail.com>
2023-05-30 16:17:40 +0900

    Add MagicAptosWallet

--- packages/@magic-ext/aptos/src/index.ts
+++ packages/@magic-ext/aptos/src/index.ts
@@ -2,7 +2,8 @@ import { Extension } from '@magic-sdk/commons';
 
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
-import { BCS } from 'aptos';
+import { BCS, TxnBuilderTypes, Types } from 'aptos';
+import { AccountInfo, NetworkInfo, SignMessagePayload, SignMessageResponse } from '@aptos-labs/wallet-adapter-core';
 import { AptosConfig, AptosPayloadMethod } from './type';
 
 export class AptosExtension extends Extension.Internal<'aptos', any> {
@@ -44,5 +45,47 @@ export class AptosExtension extends Extension.Internal<'aptos', any> {
     return this.request<Uint8Array>(
       this.utils.createJsonRpcRequestPayload(AptosPayloadMethod.AptosSignTransaction, [serialized]),
     );
+  };
+
+  connect = (): Promise<AccountInfo> => {
+    throw new Error('Method not implemented.');
+  };
+
+  account = (): Promise<AccountInfo> => {
+    throw new Error('Method not implemented.');
+  };
+
+  disconnect = (): Promise<void> => {
+    throw new Error('Method not implemented.');
+  };
+
+  signAndSubmitTransaction = (
+    transaction: Types.TransactionPayload,
+    options?: any,
+  ): Promise<{ hash: Types.HexEncodedBytes }> => {
+    throw new Error('Method not implemented.');
+  };
+
+  signAndSubmitBCSTransaction = (
+    transaction: TxnBuilderTypes.TransactionPayload,
+    options?: any,
+  ): Promise<{ hash: Types.HexEncodedBytes }> => {
+    throw new Error('Method not implemented.');
+  };
+
+  signMessage = (message: SignMessagePayload): Promise<SignMessageResponse> => {
+    throw new Error('Method not implemented.');
+  };
+
+  network = (): Promise<NetworkInfo> => {
+    throw new Error('Method not implemented.');
+  };
+
+  onNetworkChange = (callback: any): Promise<void> => {
+    throw new Error('Method not implemented.');
+  };
+
+  onAccountChange = (callback: any): Promise<void> => {
+    throw new Error('Method not implemented.');
   };
 }
