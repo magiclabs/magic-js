d215fd18b766ff9b96a190bd2ece5af259c9e4bb
Arian Flores <arian.flores@magic.link>
2023-06-22 12:28:09 -0700

    Adds new @magic-ext/react-native-expo-oidc package

---
+++ packages/@magic-ext/react-native-expo-oidc/CHANGELOG.md
@@ -0,0 +1,17 @@
+# v1.0.1 (Wed Jan 04 2023)
+
+#### 🐛 Bug Fix
+
+- add oidc support [#349](https://github.com/magiclabs/magic-js/pull/349) ([@dgerrellsMagic](https://github.com/dgerrellsMagic))
+
+#### Authors: 1
+
+- [@dgerrellsMagic](https://github.com/dgerrellsMagic)
+
+---
+
+# v1.0.0 (Tue Aug 16 2022)
+
+#### Added
+
+- Magic OIDC
---
+++ packages/@magic-ext/react-native-expo-oidc/src/index.ts
@@ -0,0 +1,16 @@
+import { Extension } from '@magic-sdk/react-native-expo';
+import { MagicOpenIdConnectPayloadMethod, LoginWithOpenIdParams } from './types';
+
+export class OpenIdExtension extends Extension.Internal<'openid', any> {
+  name = 'openid' as const;
+  config: any = {};
+
+  public loginWithOIDC(params: LoginWithOpenIdParams) {
+    const requestPayload = this.utils.createJsonRpcRequestPayload(MagicOpenIdConnectPayloadMethod.LoginWithOIDC, [
+      params,
+    ]);
+    return this.request<string>(requestPayload);
+  }
+}
+
+export * from './types';
