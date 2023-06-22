6b1acc6b1636ab3e7977badaaa1972267ecfab25
Arian Flores <arian.flores@magic.link>
2023-06-22 15:05:18 -0700

    Remove new packages

--- packages/@magic-ext/react-native-bare-oidc/src/types.ts
+++
@@ -1,8 +0,0 @@
-export interface LoginWithOpenIdParams {
-  jwt: string;
-  providerId: string;
-}
-
-export enum MagicOpenIdConnectPayloadMethod {
-  LoginWithOIDC = 'magic_auth_login_with_oidc',
-}
