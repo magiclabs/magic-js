2002403583e5928a8c8ace61cf727b6abdf1089e
David He <david.he@magic.link>
2023-07-05 12:50:56 -0400

    fix provider enable test

--- packages/@magic-sdk/provider/test/spec/modules/rpc-provider/enable.spec.ts
+++ packages/@magic-sdk/provider/test/spec/modules/rpc-provider/enable.spec.ts
@@ -8,12 +8,12 @@ beforeEach(() => {
   (BaseModule as any).prototype.request = jest.fn();
 });
 
-test('Generates a JSON RPC request payload with method `eth_accounts`', async () => {
+test('Generates a JSON RPC request payload with method `mc_login`', async () => {
   const magic = createMagicSDK();
 
   magic.rpcProvider.enable();
 
   const requestPayload = magic.user.request.mock.calls[0][0];
-  expect(requestPayload.method).toBe('eth_accounts');
+  expect(requestPayload.method).toBe('mc_login');
   expect(requestPayload.params).toEqual([]);
 });
