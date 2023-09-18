537efe29bade24e1f3472d561710e8a7fb3aa1ae
Chris Yang <i@ysm.dev>
2023-09-18 17:01:53 +0900

    fix type

--- packages/@magic-ext/oauth/src/crypto.ts
+++ packages/@magic-ext/oauth/src/crypto.ts
@@ -1,4 +1,4 @@
-import { WordArray, SHA256 } from 'crypto-js';
+import SHA256 from 'crypto-js/sha256';
 import Base64 from 'crypto-js/enc-base64';
 
 const CHARSET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-._~';
@@ -18,9 +18,9 @@ function bytesToVerifierString(bytes: Uint8Array) {
  * Stringifies argument (as CryptoJS `WordArray` or EcmaScript `ArrayBuffer`)
  * and encodes to URL-safe Base64.
  */
-function base64URLEncodeFromByteArray(wordArray: WordArray): string;
+function base64URLEncodeFromByteArray(wordArray: CryptoJS.lib.WordArray): string;
 function base64URLEncodeFromByteArray(arrayBuffer: ArrayBuffer): string;
-function base64URLEncodeFromByteArray(arg: WordArray | ArrayBuffer): string {
+function base64URLEncodeFromByteArray(arg: CryptoJS.lib.WordArray | ArrayBuffer): string {
   const makeURLSafe = (base64: string) => {
     return base64.replace(/\+/g, '-').replace(/\//g, '_').replace(/=/g, '');
   };
