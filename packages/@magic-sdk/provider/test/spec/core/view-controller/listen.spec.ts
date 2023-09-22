8fc0bada52b89e18c160838a50f8a9a3ea1bf42f
Arian Flores <arian.flores@magic.link>
2023-09-21 17:19:47 -0700

    Adds more test coverage

--- packages/@magic-sdk/provider/test/spec/core/view-controller/listen.spec.ts
+++ packages/@magic-sdk/provider/test/spec/core/view-controller/listen.spec.ts
@@ -1,10 +1,17 @@
 import browserEnv from '@ikscodes/browser-env';
 import { createViewController } from '../../../factories';
 import { MSG_TYPES } from '../../../constants';
+import { createDeprecationWarning } from '../../../../src/core/sdk-exceptions';
+import { UniversalWalletRemovalVersions } from '../../../../src/core/view-controller';
 
 beforeEach(() => {
   browserEnv();
 });
+
+jest.mock('../../../../src/core/sdk-exceptions', () => ({
+  ...jest.requireActual('../../../../src/core/sdk-exceptions'),
+  createDeprecationWarning: jest.fn(),
+}));
 
 test('Receive MAGIC_HIDE_OVERLAY, call `hideOverlay`', (done) => {
   const overlay = createViewController('');
@@ -28,6 +35,37 @@ test('Receive MAGIC_SHOW_OVERLAY, call `showOverlay`', (done) => {
 
   setTimeout(() => {
     expect(showOverlayStub).toBeCalledTimes(1);
+    done();
+  }, 0);
+});
+
+test('Receive MAGIC_SEND_PRODUCT_TYPE with product_type "connect", call `createDeprecationWarning`', (done) => {
+  const overlay = createViewController('');
+
+  // Cast the imported function to its mocked version
+  const mockCreateDeprecationWarning = createDeprecationWarning as jest.Mock;
+
+  // Mock the return value
+  mockCreateDeprecationWarning.mockReturnValue({
+    log: jest.fn(),
+  });
+
+  window.postMessage(
+    {
+      msgType: MSG_TYPES().MAGIC_SEND_PRODUCT_TYPE,
+      response: {
+        product_type: 'connect',
+      },
+    },
+    '*',
+  );
+
+  setTimeout(() => {
+    expect(mockCreateDeprecationWarning).toBeCalledWith({
+      method: 'Usage of Universal Wallet API Keys',
+      removalVersions: UniversalWalletRemovalVersions,
+      useInstead: 'Dedicated Wallet API Key',
+    });
     done();
   }, 0);
 });
