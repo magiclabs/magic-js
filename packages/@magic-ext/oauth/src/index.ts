769299189fddb7629dd8d62394d15d878d92ce79
Pat Jacobs <pat.jacobs@magic.link>
2024-01-11 15:21:39 -0800

    params

--- packages/@magic-ext/oauth/src/index.ts
+++ packages/@magic-ext/oauth/src/index.ts
@@ -73,10 +73,7 @@ export class OAuthExtension extends Extension.Internal<'oauth'> {
 
   public loginWithPopup(configuration: OAuthPopupConfiguration) {
     console.log('loginWithPopup', configuration);
-    window.open(
-      `http://localhost:3014/v1/oauth2/popup/${configuration.provider}/start?provider=${configuration.provider}`,
-      '_blank',
-    );
+    window.open(`http://localhost:3014/v1/oauth2/popup/${configuration.provider}/start`, '_blank');
   }
 
   public getRedirectResult() {
