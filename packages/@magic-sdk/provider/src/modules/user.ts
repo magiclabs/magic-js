12b869f0f29761f622bc7ff80ab6addcf7b5b92b
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2024-04-01 09:51:20 -0700

    Revert "test"
    This reverts commit 96543fc44e7f9bffeda1b8af7326fd56e9459f27.

--- packages/@magic-sdk/provider/src/modules/user.ts
+++ packages/@magic-sdk/provider/src/modules/user.ts
@@ -124,11 +124,6 @@ export class UserModule extends BaseModule {
     return this.request<boolean>(requestPayload);
   }
 
-  public showTypes() {
-    console.log('MagicPayloadMethods: ', MagicPayloadMethod);
-    console.log('RevealPayloadMethod: ', MagicPayloadMethod.RevealPrivateKey);
-  }
-
   // Deprecating
   public getMetadata() {
     createDeprecationWarning({
