169a4eab13d8b0c78a440d507533c1810eb175d7
romin-halltari <romin.halltari@magic.link>
2024-06-11 13:18:57 -0700

    fix: add logs

--- packages/@magic-sdk/provider/src/util/storage.ts
+++ packages/@magic-sdk/provider/src/util/storage.ts
@@ -1,4 +1,5 @@
 import { SDKEnvironment } from '../core/sdk-environment';
+import { logInfo } from '../core/datadog';
 
 let lf: LocalForage;
 
@@ -8,8 +9,11 @@ let lf: LocalForage;
 function proxyLocalForageMethod<TMethod extends keyof LocalForageDbMethods>(method: TMethod): LocalForage[TMethod] {
   return async (...args: any[]) => {
     /* istanbul ignore else */
+    logInfo('proxyLocalForageMethod starting');
     if (!lf) lf = await SDKEnvironment.configureStorage();
+    logInfo('proxyLocalForageMethod waiting for ready');
     await lf.ready();
+    logInfo('proxyLocalForageMethod is ready!!!');
     return (lf[method] as any)(...args);
   };
 }
