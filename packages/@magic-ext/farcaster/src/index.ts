c835ac2c8cec2be66678aaaa9292ae1b59bf18a2
Chris Yang <i@ysm.dev>
2024-06-28 04:08:52 +0900

    fix: fix mobile, move channel logic to magic-ext/farcaster

--- packages/@magic-ext/farcaster/src/index.ts
+++ packages/@magic-ext/farcaster/src/index.ts
@@ -24,8 +24,33 @@ type FarcasterLoginEventHandlers = {
 export class FarcasterExtension extends Extension.Internal<'farcaster'> {
   name = 'farcaster' as const;
   config = {};
+  channel: CreateChannelAPIResponse | null = null;
+
+  constructor() {
+    super();
+
+    (async () => {
+      const json = await fetch(`https://relay.farcaster.xyz/v1/channel`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          domain: location.host,
+          siweUri: location.origin,
+        }),
+      }).then<CreateChannelAPIResponse>((r) => r.json());
+
+      this.channel = json;
+    })();
+  }
 
   public login = (params?: LoginParams) => {
+    if (!this.channel) {
+      console.info('Channel not created yet. Please wait for the channel to be created.');
+      return;
+    }
+
     const showUI = params?.showUI ?? DEFAULT_SHOW_UI;
 
     const domain = location.origin;
@@ -36,6 +61,7 @@ export class FarcasterExtension extends Extension.Internal<'farcaster'> {
           showUI,
           domain,
           isMobile: isMobile(),
+          channel: this.channel,
         },
       },
     ]);
@@ -43,9 +69,7 @@ export class FarcasterExtension extends Extension.Internal<'farcaster'> {
     const handle = this.request<string, FarcasterLoginEventHandlers>(payload);
 
     if (isMobile()) {
-      handle.on('channel', (channel) => {
-        window.open(channel.url, '_blank');
-      });
+      location.href = this.channel.url;
     }
 
     return handle;
