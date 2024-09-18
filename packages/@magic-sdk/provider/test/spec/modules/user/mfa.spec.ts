8d4799b3510e5d3f3d7de185877fe87fca3b26d2
sherzod-bakhodirov <sherzod.bakhodirov@magic-ext.com>
2024-09-18 11:49:50 +0500

    chore: update mfa tests

--- packages/@magic-sdk/provider/test/spec/modules/user/mfa.spec.ts
+++ packages/@magic-sdk/provider/test/spec/modules/user/mfa.spec.ts
@@ -1,3 +1,5 @@
+import { DisableMFAEventEmit, DisableMFAEventOnReceived } from '@magic-sdk/types/dist/types';
+import { EnableMFAEventEmit } from 'magic-sdk';
 import { createMagicSDK } from '../../../factories';
 
 test('Resolves immediately when cached magic.user.enableMFA.spec is true with ShowUI: true', async () => {
@@ -33,15 +35,24 @@ test('Resolves immediately when cached magic.user.enableMFA.spec is true with em
   expect(requestPayload.params).toEqual([{ showUI: true }]);
 });
 
-test('Resolves immediately when cached magic.user.enableMFA.spec is true without any params', async () => {
+test('Enable MFA flow returns invalid mfa event', () => {
   const magic = createMagicSDK();
-  magic.user.request = jest.fn();
+  magic.user.overlay.post = jest.fn().mockImplementation(() => new Promise(() => {}));
+  const createIntermediaryEventFn = jest.fn();
+  magic.user.createIntermediaryEvent = jest.fn().mockImplementation(() => createIntermediaryEventFn);
 
-  await magic.user.enableMFA();
+  const handle = magic.user.enableMFA({ showUI: false });
 
-  const requestPayload = magic.user.request.mock.calls[0][0];
-  expect(requestPayload.method).toBe('magic_auth_enable_mfa_flow');
-  expect(requestPayload.params).toEqual([{ showUI: true }]);
+  const troll_otp = '123456';
+  handle.emit('verify-mfa-code', troll_otp);
+  handle.emit('cancel-mfa-setup');
+
+  const verifyEvent = magic.user.createIntermediaryEvent.mock.calls[0];
+  expect(verifyEvent[0]).toBe('verify-mfa-code');
+  expect(createIntermediaryEventFn.mock.calls[0][0]).toBe(troll_otp);
+
+  const intermediaryEventSecondMethod = magic.user.createIntermediaryEvent.mock.calls[1][0];
+  expect(intermediaryEventSecondMethod).toBe('cancel-mfa-setup');
 });
 
 test('Resolves immediately when cached magic.user.enableMFA.spec is true with ShowUI: true', () => {
@@ -77,13 +88,28 @@ test('Resolves immediately when cached magic.user.enableMFA.spec is true with em
   expect(requestPayload.params).toEqual([{ showUI: true }]);
 });
 
-test('Resolves immediately when cached magic.user.enableMFA.spec is true without any params', () => {
+test('Disable MFA flow returns invalid mfa event', () => {
   const magic = createMagicSDK();
-  magic.user.request = jest.fn();
+  magic.user.overlay.post = jest.fn().mockImplementation(() => new Promise(() => {}));
+  const createIntermediaryEventFn = jest.fn();
+  magic.user.createIntermediaryEvent = jest.fn().mockImplementation(() => createIntermediaryEventFn);
 
-  magic.user.disableMFA();
+  const handle = magic.user.disableMFA({ showUI: false });
 
-  const requestPayload = magic.user.request.mock.calls[0][0];
-  expect(requestPayload.method).toBe('magic_auth_disable_mfa_flow');
-  expect(requestPayload.params).toEqual([{ showUI: true }]);
+  const troll_otp = '123456';
+  handle.emit('verify-mfa-code', troll_otp);
+  handle.emit('cancel-mfa-disable');
+  handle.emit('lost-device', troll_otp);
+
+  const events = magic.user.createIntermediaryEvent.mock.calls;
+
+  const verifyEvent = events[0][0];
+  expect(verifyEvent).toBe('verify-mfa-code');
+  expect(createIntermediaryEventFn.mock.calls[0][0]).toBe(troll_otp);
+
+  const cancelEvent = events[1][0];
+  expect(cancelEvent).toBe('cancel-mfa-disable');
+
+  const lostDeviceEvent = events[2][0];
+  expect(lostDeviceEvent).toBe('lost-device');
 });
