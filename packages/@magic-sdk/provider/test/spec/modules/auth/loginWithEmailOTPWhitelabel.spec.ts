bca4feffbbb26581e7f8471c431450b9d9d9ac07
sherzod-bakhodirov <sherzod.bakhodirov@magic-ext.com>
2024-10-22 17:13:35 +0500

    feat: add tests for login with email otp whitelabel

--- packages/@magic-sdk/provider/test/spec/modules/auth/loginWithEmailOTPWhitelabel.spec.ts
+++ packages/@magic-sdk/provider/test/spec/modules/auth/loginWithEmailOTPWhitelabel.spec.ts
@@ -47,3 +47,23 @@ test('Generates JSON RPC pending for verify-mfa-code', () => {
   const intermediaryEventSecondMethod = magic.auth.createIntermediaryEvent.mock.calls[1][0];
   expect(intermediaryEventSecondMethod).toBe('cancel');
 });
+
+test('Generates JSON RPC pending for lost-device', () => {
+  const magic = createMagicSDK();
+  magic.auth.overlay.post = jest.fn().mockImplementation(() => new Promise(() => {}));
+  const createIntermediaryEventFn = jest.fn();
+  magic.auth.createIntermediaryEvent = jest.fn().mockImplementation(() => createIntermediaryEventFn);
+
+  const handle = magic.auth.loginWithEmailOTP({ email: expectedEmail, showUI: false });
+
+  const recovery_code = '10epf6fk';
+  handle.emit('lost-device', recovery_code);
+  handle.emit('cancel');
+
+  const verifyEvent = magic.auth.createIntermediaryEvent.mock.calls[0];
+  expect(verifyEvent[0]).toBe('lost-device');
+  expect(createIntermediaryEventFn.mock.calls[0][0]).toBe(recovery_code);
+
+  const intermediaryEventSecondMethod = magic.auth.createIntermediaryEvent.mock.calls[1][0];
+  expect(intermediaryEventSecondMethod).toBe('cancel');
+});
