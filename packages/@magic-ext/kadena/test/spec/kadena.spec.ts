6e73afed2142be9ed0f89e020b8a1e357a2df716
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2024-11-12 17:54:01 -0800

    chore: adds tests

--- packages/@magic-ext/kadena/test/spec/kadena.spec.ts
+++ packages/@magic-ext/kadena/test/spec/kadena.spec.ts
@@ -27,3 +27,39 @@ test('Sends params as payload', () => {
   expect(requestPayload.method).toBe(KadenaPayloadMethod.KadenaSignTransaction);
   expect(requestPayload.params).toEqual([{ hash: '0x123' }]);
 });
+
+test('Generate JSON RPC request payload with method `kda_loginWithSpireKey`', () => {
+  const magic = createMagicSDKWithExtension({}, [
+    new KadenaExtension({
+      rpcUrl: '',
+      chainId: '1',
+      networkId: 'testnet04',
+      network: 'testnet',
+      createAccountsOnChain: true,
+    }),
+  ]);
+  magic.kadena.request = jest.fn();
+
+  magic.kadena.loginWithSpireKey();
+
+  const requestPayload = magic.kadena.request.mock.calls[0][0];
+  expect(requestPayload.method).toBe(KadenaPayloadMethod.KadenaLoginWithSpireKey);
+});
+
+test('Generate JSON RPC request payload with method `kda_getInfo`', () => {
+  const magic = createMagicSDKWithExtension({}, [
+    new KadenaExtension({
+      rpcUrl: '',
+      chainId: '1',
+      networkId: 'testnet04',
+      network: 'testnet',
+      createAccountsOnChain: true,
+    }),
+  ]);
+  magic.kadena.request = jest.fn();
+
+  magic.kadena.getInfo();
+
+  const requestPayload = magic.kadena.request.mock.calls[0][0];
+  expect(requestPayload.method).toBe(KadenaPayloadMethod.KadenaGetInfo);
+});
