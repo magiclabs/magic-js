07814806f93c348dc3e4fe7c9a0d73c2d458083e
Josh Scanlan <joshuascan@gmail.com>
2025-10-03 09:59:16 -0700

    removes pnp

--- packages/@magic-sdk/pnp/src/types.ts
+++
@@ -1,8 +0,0 @@
-import type { SDKBase, InstanceWithExtensions, MagicSDKExtensionsOption } from '@magic-sdk/provider';
-import type { OAuthExtension } from '@magic-ext/oauth2';
-
-export type Magic<TExtensions extends MagicSDKExtensionsOption = MagicSDKExtensionsOption> = InstanceWithExtensions<
-  SDKBase,
-  TExtensions
->;
-export type { OAuthExtension };
