ef2002944f9a47d1933be01750c412be4b2fdb55
Josh Scanlan <joshuascan@gmail.com>
2025-10-23 11:49:01 -0700

    fix build

--- packages/@magic-ext/siwe/src/index.cdn.ts
+++ packages/@magic-ext/siwe/src/index.cdn.ts
@@ -1,1 +1,8 @@
-export * from './src/index';
+import { SiweExtension } from './index';
+import * as types from './types';
+
+Object.assign(SiweExtension, {
+  ...types,
+});
+
+export default SiweExtension;
