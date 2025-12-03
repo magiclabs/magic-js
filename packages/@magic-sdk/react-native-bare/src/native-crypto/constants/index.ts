f2ac87759d234f030f6028ad2dc6625f8f7cd648
sherzod-bakhodirov <sherzod.bakhodirov@magic-ext.com>
2025-12-03 18:31:36 +0500

    chore: remove unused var

--- packages/@magic-sdk/react-native-bare/src/native-crypto/constants/index.ts
+++ packages/@magic-sdk/react-native-bare/src/native-crypto/constants/index.ts
@@ -1,4 +1,2 @@
-// Unique alias suffix to isolate SDK keys from the rest of the app
-export const DPOP_KEY_ALIAS_SUFFIX = 'magiclink.dpop';
 export const ALG = 'ES256';
 export const TYP = 'dpop+jwt';
