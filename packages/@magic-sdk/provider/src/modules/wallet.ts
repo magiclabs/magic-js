47a5a04e14c929af20c7a51021b9496b64be40e8
Josh Scanlan <joshuascan@gmail.com>
2026-01-16 14:17:16 -0800

    test

--- packages/@magic-sdk/provider/src/modules/wallet.ts
+++ packages/@magic-sdk/provider/src/modules/wallet.ts
@@ -20,6 +20,7 @@ export class WalletModule extends BaseModule {
   /* Prompt Magic's Login Form */
   public connectWithUI(options?: ConnectWithUIOptions) {
     const promiEvent = createPromiEvent<string[], ConnectWithUiEvents>(async (resolve, reject) => {
+      console.log('test')
       try {
         const loginRequestPayload = createJsonRpcRequestPayload(MagicPayloadMethod.Login, [
           {
