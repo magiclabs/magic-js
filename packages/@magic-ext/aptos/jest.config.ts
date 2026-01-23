2763d99cc4c8e5644f75cc63067deab16aa44472
Jerry Liu <33166884+Ethella@users.noreply.github.com>
2026-01-23 15:20:23 -0800

    bump versions (#985)
    * address security vulns
    
    * chore: update yarn.lock
    
    * feat: mock aptos-labs/wallet-adapter-core & expo application
    
    ---------
    
    Co-authored-by: sherzod-bakhodirov <sherzod.bakhodirov@magic-ext.com>

--- packages/@magic-ext/aptos/jest.config.ts
+++ packages/@magic-ext/aptos/jest.config.ts
@@ -8,6 +8,10 @@ const config: Config.InitialOptions = {
     '\\.(ts|tsx)$': 'ts-jest',
   },
   coveragePathIgnorePatterns: ['index.cdn.ts', 'index.native.ts'],
+  moduleNameMapper: {
+    '^@aptos-labs/wallet-adapter-core$': '<rootDir>/test/__mocks__/@aptos-labs/wallet-adapter-core.ts',
+    '^@aptos-labs/wallet-standard$': '<rootDir>/test/__mocks__/@aptos-labs/wallet-standard.ts',
+  },
 };
 
 export default config;
