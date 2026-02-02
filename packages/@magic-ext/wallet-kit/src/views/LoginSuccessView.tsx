72060a05ab386955c3ef51351d9ff3def871a05f
Josh Scanlan <joshuascan@gmail.com>
2026-02-02 11:06:12 -0800

    rename package

--- packages/@magic-ext/wallet-kit/src/views/LoginSuccessView.tsx
+++ packages/@magic-ext/wallet-kit/src/views/LoginSuccessView.tsx
@@ -16,7 +16,7 @@ export const LoginSuccessView = ({ state }: LoginSuccessViewProps) => {
     <>
       <WidgetHeader showHeaderText={false} />
       <VStack gap={6}>
-        <IcoCheckmarkCircleFill width={60} height={60} color={token('colors.brand.base')} />
+        <IcoCheckmarkCircleFill width={40} height={40} color={token('colors.brand.base')} />
 
         <VStack gap={1}>
           <Text fontColor="text.tertiary" styles={{ textAlign: 'center' }}>
