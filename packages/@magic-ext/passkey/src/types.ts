59d8364ed998d98ccaa9b5a0c29e7be277286e39
sherzod-bakhodirov <sherzod.bakhodirov@magic-ext.com>
2026-06-25 03:22:48 +0500

    feat: implement adding passkey to existing users (#1096)
    * feat: implement adding passkey to existing users
    
    * chore: rename credentialId to passkeyId

--- packages/@magic-ext/passkey/src/types.ts
+++ packages/@magic-ext/passkey/src/types.ts
@@ -26,15 +26,52 @@ export interface LoginWithPasskeyConfiguration {
   lifespan?: number;
 }
 
+export interface AddPasskeyConfiguration {
+  /**
+   * The username of the user adding the passkey.
+   * Optional parameter used as display name for the passkey.
+   */
+  username?: string;
+
+  /**
+   * The nickname that the user attempts to set to this passkey.
+   */
+  nickname?: string;
+}
+
+export interface UpdatePasskeyConfiguration {
+  /**
+   * The passkey ID of the passkey to update.
+   */
+  passkeyId: string;
+
+  /**
+   * The new nickname for the passkey.
+   */
+  nickname: string;
+}
+
+export interface RemovePasskeyConfiguration {
+  /**
+   * The passkey ID of the passkey to remove.
+   */
+  passkeyId: string;
+}
+
 export enum MagicPasskeyPayloadMethod {
   RegisterPasskeyStart = 'magic_auth_register_passkey_start',
   RegisterPasskeyVerify = 'magic_auth_register_passkey_verify',
   LoginWithPasskeyStart = 'magic_auth_login_with_passkey_start',
   LoginWithPasskeyVerify = 'magic_auth_login_with_passkey_verify',
   GetPasskeyInfo = 'magic_user_get_webauthn_credentials',
+  AddPasskeyStart = 'magic_auth_add_passkey_start',
+  AddPasskeyVerify = 'magic_auth_add_passkey_verify',
+  UpdatePasskey = 'magic_auth_update_passkey',
+  RemovePasskey = 'magic_auth_remove_passkey',
 }
 
 export enum PasskeySDKErrorCode {
   PasskeyNotSupported = 'PASSKEY_NOT_SUPPORTED',
   PasskeyRegisterError = 'PASSKEY_REGISTRATION_ERROR',
+  PasskeyUserCancelledOrTimeout = 'PASSKEY_USER_CANCELLED_OR_TIMEOUT',
 }
