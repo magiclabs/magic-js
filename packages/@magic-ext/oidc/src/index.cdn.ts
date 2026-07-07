73d4be93ccb96399b9e52f26c1d1d27fbe15a65c
ethella <jerry@magic.link>
2026-07-07 11:59:42 -0700

    remove oidc packages

--- packages/@magic-ext/oidc/src/index.cdn.ts
+++
@@ -1,8 +0,0 @@
-import { OpenIdExtension } from './index';
-import * as types from './types';
-
-Object.assign(OpenIdExtension, {
-  ...types,
-});
-
-export default OpenIdExtension;
--- packages/@magic-ext/oidc/src/types.ts
+++
@@ -1,9 +0,0 @@
-export interface LoginWithOpenIdParams {
-  jwt: string;
-  providerId: string;
-  lifespan?: number;
-}
-
-export enum MagicOpenIdConnectPayloadMethod {
-  LoginWithOIDC = 'magic_auth_login_with_oidc',
-}
